f8844818b975d636c258d9d23fe8b47e99e834cd
linarcx <linarcx@riseup.net>
2022-04-29 22:50:00 +0430

    Fixed crash when run it in virtualbox, adding types.hpp

--- sources/cpp/kernel.cpp
+++ sources/cpp/kernel.cpp
@@ -1,26 +1,22 @@
-void printf(char* str)
-{
-  unsigned short* VideoMemory = (unsigned short*)0xb8000;
-  for(int i = 0; str[i]!='\0'; i++)
-  {
-      VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-  }
-}
+#include "types.hpp"
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
-
 extern "C" void callConstructors()
 {
-  for(constructor* i = &start_ctors; i != (void (**)())end_ctors; i++)
-  {
+  for(constructor* i = &start_ctors; i != &end_ctors; i++)
     (*i)();
-  }
-
 }
 
-extern "C" void kernelMain(void* multiboot_structor, unsigned int magic_number)
+void printf(const char* str)
+{
+  uint16_t* VideoMemory = (uint16_t*)0xb8000;
+  for(int32_t i = 0; str[i]!='\0'; i++)
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+extern "C" void kernelMain(void* multiboot_structor, uint32_t magic_number)
 {
   printf("Hello, this is esperos :)");
   while(1);
